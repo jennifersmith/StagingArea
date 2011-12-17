578fd225361ca842f2c91fb7500e6d518b1de93c
Jennifer Smith <jennifer@home>
2011-12-17 09:28:17 -0600

    Bit of last minute rfing

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cdc_server "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.0"]])
+                 [org.clojure/core.logic "0.6.6"]
+                 [ring "1.0.0"] [cheshire "2.0.4"]
+                ])
--- src/cdc_server/core.clj
+++ src/cdc_server/core.clj
@@ -1,6 +1,6 @@
 (ns cdc_server.core
-  (
-   :use ring.adapter.jetty))
+  (:require [clojure.xml :as xml])
+  (:use [ ring.adapter.jetty ] [cheshire.core]))
 
 (def contracts
   [
@@ -8,38 +8,69 @@
     :request-method :get
     :uri "/accounts"
     :content-type :json
-    :should-match "$.accounts.account"
+    :should-match "$.accounts.account_id"
+    :sample-data [{:accounts [{:account_id "[A-Z]+" }]}]  ;; HACK!!
+    }
+   {
+    :request-method :get
+    :uri "/statuses/user_timeline.xml"
+    :content-type :xml
+    :should-match "statuses/status/text"
+    :sample-data (xml/parse "http://twitter.com/statuses/user_timeline.xml?id=jennifersmithco")
     }
    ])
+(def content-type->http-header
+  {
+   :xml "text/xml"
+   :json "application/json"
+   })
 
-(defn matching-contracts [request]
-  (first  (filter
-           #(and
-             (= (% :uri)  (request :uri) )
-             (= (% :request-method) (request :request-method) ))
-           contracts
-           )))
+(def content-type->generator
+  {
+   :xml xml/emit
+   :json generate-string
+   })
 
-(defn mock-response [req contract]
+(defn make-match-predicate [contract]
+  (fn [request]
+    (first  (filter
+             #(and
+               (= (% :uri)  (request :uri) )
+               (= (% :request-method) (request :request-method) ))
+             contracts
+             ))))
+(defn make-mock-handler [contract]
+  (fn [req]
     {:status  200
-     :headers {"Content-Type" "text/html"}
-     :body   "foo"}
-    )
+     :headers {"Content-Type" (content-type->http-header (:content-type contract))}
+     :body  ((content-type->generator
+              (:content-type contract)) (:sample-data contract))}))
 
+(defn make-mock-handlers [contracts]
+  (conj (vec  (map
+               (fn [contract]
+                 [(make-match-predicate contract)
+                  (make-mock-handler contract)])
+               contracts
+               ))
+         [(fn [req] true) four-oh-four]
+         ))
 
 (defn four-oh-four [req]
   {
    :status 200
    :headers {"Content-Type" "text/html"}
-   :body (str req)  })
+   :body
+   (str "<h1>Page not found</h1> <textarea style=\"height:400px; width:800px\"> "  req "</textarea>" )  })
 
-(defn app [req]
-  (let [contract (matching-contracts req)]
-   (if (nil? contract )      
-      (four-oh-four req)
-      (mock-response req contract))))
+(defn app [request]
+  (first
+   (filter (comp not nil?)
+           (map (fn [[match? handler]]
+                  (if (match? request) (handler request)))
+                (make-mock-handlers contracts)))))
 
-;;(def server (run-jetty #'app {:port 9001 :join? false}))
+(def server (run-jetty #'app {:port 9007 :join? false}))
 
 (defn restart-server []
   (.stop server)
